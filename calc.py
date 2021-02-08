a3bba04f10f06cc5525123cb6a101688bc589e0d
mSE26 <Nikif1996@mail.ru>
2021-02-08 16:35:30 +0900

    dev2

--- calc.py
+++ calc.py
@@ -1,8 +1,21 @@
+operation = input('''
+Введите математическую операцию, которую вы хотите выполнить:
++ 
+- 
+* 
+/ 
+''')
+
 num1 = int(input('Первая цифра: '))
 num2 = int(input('Вторая цифра: '))
 
-print('{} + {} = '.format(num1, num2))
-print(num1 + num2)
+if operation == '+':
+    print('{} + {} = '.format(num1, num2))
+    print(num1 + num2)
 
-print('{} - {} = '.format(num1, num2))
-print(num1 - num2)
+elif operation == '-':
+    print('{} - {} = '.format(num1, num2))
+    print(num1 - num2)
+
+else:
+    print('Вы не ввели правильный оператор, запустите программу еще раз.')

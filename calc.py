8836f20206db3eab66e940796ebc589bf59f74e8
mSE26 <Nikif1996@mail.ru>
2021-02-08 16:50:17 +0900

    devFin

--- calc.py
+++ calc.py
@@ -1,4 +1,5 @@
-operation = input('''
+def calc():
+    operation = input('''
 Введите математическую операцию, которую вы хотите выполнить:
 + 
 - 
@@ -6,24 +7,41 @@ operation = input('''
 / 
 ''')
 
-num1 = int(input('Первая цифра: '))
-num2 = int(input('Вторая цифра: '))
+    num1 = int(input('Первая цифра: '))
+    num2 = int(input('Вторая цифра: '))
 
-if operation == '+':
-    print('{} + {} = '.format(num1, num2))
-    print(num1 + num2)
+    if operation == '+':
+        print('{} + {} = '.format(num1, num2))
+        print(num1 + num2)
 
-elif operation == '-':
-    print('{} - {} = '.format(num1, num2))
-    print(num1 - num2)
+    elif operation == '-':
+        print('{} - {} = '.format(num1, num2))
+        print(num1 - num2)
 
-elif operation == '*':
-    print('{} * {} = '.format(num1, num2))
-    print(num1 * num2)
+    elif operation == '*':
+        print('{} * {} = '.format(num1, num2))
+        print(num1 * num2)
 
-elif operation == '/':
-    print('{} / {} = '.format(num1, num2))
-    print(num1 / num2)
+    elif operation == '/':
+        print('{} / {} = '.format(num1, num2))
+        print(num1 / num2)
 
-else:
-    print('Вы не ввели правильный оператор, запустите программу еще раз.')
+    else:
+        print('Вы не ввели правильный оператор, запустите программу еще раз.')
+    
+    again()
+
+def again():
+    calc_again = input('''
+Хотите еще раз посчитать?
+Пожалуйста, введите Y для ДА или N для НЕТ.
+''')
+
+    if calc_again.upper() == 'Y':
+        calc()
+    elif calc_again.upper() == 'N':
+        print('До скорого.')
+    else:
+        again()
+
+calc()
